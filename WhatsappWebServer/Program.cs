af9e0b6d36237c17548a95fed25209117497bcb6
nikolEstrin <nikolestrin@gmail.com>
2022-05-19 09:07:43 +0300

    fixed fetch problem

--- WhatsappWebServer/Program.cs
+++ WhatsappWebServer/Program.cs
@@ -11,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Allow All",
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
@@ -20,6 +28,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("Allow All");
 
 app.UseHttpsRedirection();
 
